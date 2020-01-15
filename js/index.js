c6b1f3fbf0cad6f5347466f71b859b67f032d6a9
Nabeelah <noblenab13@gmail.com>
2020-01-15 16:16:01 +0100

    beach ball draggable

--- js/index.js
+++ js/index.js
@@ -1,54 +1,54 @@
 // Your code goes here
 // [ x] `mouseover`-(Mouse enter and mouse leave destination sections- updates pick your dest picture)
-const hoverSunPic = document.querySelectorAll(".destination .btn")[0];
+const hoverSunPic = document.querySelectorAll('.destination .btn')[0];
 
-hoverSunPic.addEventListener("mouseover", event => {
-  document.querySelector("#destinationImg").src = "img/sun.jpeg";
-  document.querySelector("#destinationImg").style.width = "600px";
-  document.querySelector("#destinationImg").style.height = "200px";
+hoverSunPic.addEventListener('mouseover', event => {
+  document.querySelector('#destinationImg').src = 'img/sun.jpeg';
+  document.querySelector('#destinationImg').style.width = '600px';
+  document.querySelector('#destinationImg').style.height = '200px';
   event.stopPropagation();
 });
 
-const hoverIslandPic = document.querySelectorAll(".destination .btn")[2];
+const hoverIslandPic = document.querySelectorAll('.destination .btn')[2];
 
-hoverIslandPic.addEventListener("mouseover", event => {
-  document.querySelector("#destinationImg").src = "img/island.jpg";
-  document.querySelector("#destinationImg").style.width = "600px";
-  document.querySelector("#destinationImg").style.height = "200px";
+hoverIslandPic.addEventListener('mouseover', event => {
+  document.querySelector('#destinationImg').src = 'img/island.jpg';
+  document.querySelector('#destinationImg').style.width = '600px';
+  document.querySelector('#destinationImg').style.height = '200px';
   event.stopPropagation();
 });
-hoverIslandPic.addEventListener("mouseleave", event => {
-  document.querySelector("#destinationImg").src = "img/destination.jpg";
-  document.querySelector("#destinationImg").style.width = "600px";
-  document.querySelector("#destinationImg").style.height = "200px";
+hoverIslandPic.addEventListener('mouseleave', event => {
+  document.querySelector('#destinationImg').src = 'img/destination.jpg';
+  document.querySelector('#destinationImg').style.width = '600px';
+  document.querySelector('#destinationImg').style.height = '200px';
   event.stopPropagation();
 });
 
-const hoverMountainPic = document.querySelectorAll(".destination .btn")[1];
+const hoverMountainPic = document.querySelectorAll('.destination .btn')[1];
 
-hoverMountainPic.addEventListener("mouseover", event => {
-  document.querySelector("#destinationImg").src = "img/mountain.jpeg";
-  document.querySelector("#destinationImg").style.width = "600px";
-  document.querySelector("#destinationImg").style.height = "200px";
+hoverMountainPic.addEventListener('mouseover', event => {
+  document.querySelector('#destinationImg').src = 'img/mountain.jpeg';
+  document.querySelector('#destinationImg').style.width = '600px';
+  document.querySelector('#destinationImg').style.height = '200px';
   event.stopPropagation();
 });
-hoverMountainPic.addEventListener("mouseleave", event => {
-  document.querySelector("#destinationImg").src = "img/destination.jpg";
-  document.querySelector("#destinationImg").style.width = "600px";
-  document.querySelector("#destinationImg").style.height = "200px";
+hoverMountainPic.addEventListener('mouseleave', event => {
+  document.querySelector('#destinationImg').src = 'img/destination.jpg';
+  document.querySelector('#destinationImg').style.width = '600px';
+  document.querySelector('#destinationImg').style.height = '200px';
 
   event.stopPropagation();
 });
 
 //[ x] `keydown`
-const lastNameInput = document.getElementsByName("lname")[0];
+const lastNameInput = document.getElementsByName('lname')[0];
 
-lastNameInput.addEventListener("keydown", event => {
+lastNameInput.addEventListener('keydown', event => {
   console.log(`User first name entered: ${event.key}`);
 });
-const firstNameInput = document.getElementsByName("fname")[0];
+const firstNameInput = document.getElementsByName('fname')[0];
 
-firstNameInput.addEventListener("keydown", event => {
+firstNameInput.addEventListener('keydown', event => {
   console.log(`User last name entered: ${event.key}`);
 });
 // [ x] `wheel`. Increase decreases size of bottom picture on mouse wheel
@@ -61,34 +61,60 @@ function zoom(event) {
 }
 
 let scale = 1;
-const el = document.querySelector("#destinationImg");
-const wheel = document.querySelector("div");
+const el = document.querySelector('#destinationImg');
+const wheel = document.querySelector('div');
 el.onwheel = zoom;
 
-wheel.addEventListener("wheel", zoom);
+wheel.addEventListener('wheel', zoom);
 
 // [ ] `drag / drop`
 
-const beachball = document.querySelector("#beachball");
+const beachball = document.getElementById('beachball');
+const org = document.getElementById('droptarget');
+const dropDiv = document.getElementById('drag1');
 
-beachball.addEventListener("dragstart", function(event) {
-  event.dataTransfer.setData("text", event.target.id);
-});
-beachball.addEventListener("dragover", function(event) {
-  event.preventDefault();
+org.addEventListener('drop', e => {
+  e.preventDefault();
+  let data = e.dataTransfer.getData('text');
+  console.log(data);
+  e.target.appendChild(document.getElementById(data));
 });
 
-beachball.addEventListener("drop", function(event) {
-  event.preventDefault();
-  console.log(event);
-  if (event.target.className == "droptarget") {
-       var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
-  }
+org.addEventListener('dragover', e => {
+  e.preventDefault();
 });
-const parasol = document.querySelector("#parasol");
+
+beachball.addEventListener('dragstart', e => {
+  e.dataTransfer.setData('text', e.target.id);
+  console.log(e);
+  // e.target.appendChild(document.getElementById(data));
+});
+
+dropDiv.addEventListener('drop', e => {
+  e.preventDefault();
+  let data = e.dataTransfer.getData('text');
+  console.log(data);
+  e.target.appendChild(document.getElementById(data));
+});
+
+dropDiv.addEventListener('dragover', e => {
+  e.preventDefault();
+});
+// beachball.addEventListener('dragover', function(event) {
+//   event.preventDefault();
+// });
+
+// beachball.addEventListener('drop', function(event) {
+//   event.preventDefault();
+//   console.log(event);
+//   if (event.target.className == 'droptarget') {
+//     var data = event.dataTransfer.getData('text');
+//     event.target.appendChild(document.getElementById(data));
+//   }
+// });
+// const parasol = document.querySelector('#parasol');
 //parasol.addEventListener("drop", myScript);
-const coconut = document.querySelector("#coconut");
+// const coconut = document.querySelector('#coconut');
 //coconut.addEventListener("drop", myScript);
 
 //[ ] `load`
@@ -96,15 +122,15 @@ const coconut = document.querySelector("#coconut");
 // [ x] `focus`
 // if passwords not the same or if email is not valid box goes red else green
 const blur = document.querySelector('input[name="email"]');
-blur.addEventListener("blur", event => {
-  if (document.querySelector("#email").value.includes("@")) {
-    document.querySelector("#email").style.backgroundColor = "green";
+blur.addEventListener('blur', event => {
+  if (document.querySelector('#email').value.includes('@')) {
+    document.querySelector('#email').style.backgroundColor = 'green';
 
     //     document.querySelector('input[name="email"]').focus();
   } else {
-    document.querySelector("#email").focus();
+    document.querySelector('#email').focus();
     console.log(`not a valid email`);
-    event.target.style.backgroundColor = "red";
+    event.target.style.backgroundColor = 'red';
   }
 });
 
@@ -125,19 +151,22 @@ blur.addEventListener("blur", event => {
 // [ ] `resize`
 
 // [x ] `scroll`-title red, top image goes sepia
-const scroll = document.querySelector(".container");
-document.addEventListener("scroll", event => {
-  turnHeaderPink.style.color = "red";
+const scroll = document.querySelector('.container');
+document.addEventListener('scroll', event => {
+  turnHeaderPink.style.color = 'red';
 });
-let sepia = document.querySelector("img");
-document.addEventListener("wheel", function() {
-  sepia.style.filter = "sepia(100%)";
-  sepia.style.transition = "1s";
+let sepia = document.querySelector('img');
+document.addEventListener('wheel', function() {
+  sepia.style.filter = 'sepia(100%)';
+  sepia.style.transition = '1s';
 });
 // [ ] `select`
 function logSelection(event) {
   const log = document.getElementById('log');
-  const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+  const selection = event.target.value.substring(
+    event.target.selectionStart,
+    event.target.selectionEnd
+  );
   log.textContent = `You selected: ${selection}`;
 }
 
@@ -145,57 +174,54 @@ const input = document.querySelector('input');
 input.addEventListener('select', logSelection);
 
 //[ x] `dblclick`
-let turnHeaderPink = document.querySelector(".logo-heading");
-document.addEventListener("dblclick", function() {
-  turnHeaderPink.style.color = "pink";
+let turnHeaderPink = document.querySelector('.logo-heading');
+document.addEventListener('dblclick', function() {
+  turnHeaderPink.style.color = 'pink';
 });
 
 //onclick nav links(login or register)reveals hidden form elements
-const register = document.querySelectorAll(".nav-link")[2];
+const register = document.querySelectorAll('.nav-link')[2];
 
-register.addEventListener("click", event => {
-  document.querySelector(".hidden-form").style.display = "flex";
-   document.querySelector(".hidden-form").style.backgroundColor = "yellow";
-  document.querySelector("#email").style.display = "block";
-  document.querySelector("#password2").style.display = "block";
+register.addEventListener('click', event => {
+  document.querySelector('.hidden-form').style.display = 'flex';
+  document.querySelector('.hidden-form').style.backgroundColor = 'yellow';
+  document.querySelector('#email').style.display = 'block';
+  document.querySelector('#password2').style.display = 'block';
   event.stopPropagation();
 
-  console.log("unhide registration form");
+  console.log('unhide registration form');
   console.log(event);
-  
 });
-const signIn = document.querySelectorAll(".nav-link")[3];
-signIn.addEventListener("click", event => {
-  document.querySelector(".hidden-form").style.display = "block";
-  document.querySelector(".hidden-form").style.backgroundColor = "turquoise";
-  document.querySelector("#email").style.display = "none";
-  document.querySelector("#password2").style.display = "none";
+const signIn = document.querySelectorAll('.nav-link')[3];
+signIn.addEventListener('click', event => {
+  document.querySelector('.hidden-form').style.display = 'block';
+  document.querySelector('.hidden-form').style.backgroundColor = 'turquoise';
+  document.querySelector('#email').style.display = 'none';
+  document.querySelector('#password2').style.display = 'none';
   event.stopPropagation();
 
-  console.log("unhide sign in form");
+  console.log('unhide sign in form');
   console.log(event);
 });
-
-
 
 // Submit
-const form = document.querySelector("form");
+const form = document.querySelector('form');
 
-form.addEventListener("submit", function(event) {
-  console.log("Form: Detected Submit Event!");
-  console.log("First Name", event.target[0].value);
-  console.log("Last Name", event.target[1].value);
-  console.log("Password", event.target[2].value);
+form.addEventListener('submit', function(event) {
+  console.log('Form: Detected Submit Event!');
+  console.log('First Name', event.target[0].value);
+  console.log('Last Name', event.target[1].value);
+  console.log('Password', event.target[2].value);
   event.preventDefault();
-  document.querySelector(".hidden-form").style.display = "none";
+  document.querySelector('.hidden-form').style.display = 'none';
 
   console.log(`form hidden again`);
 
   // Clear text from fields
-  document.getElementById("fname").value = "";
-  document.getElementById("lname").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("password").value = "";
-  document.getElementById("password2").value = "";
+  document.getElementById('fname').value = '';
+  document.getElementById('lname').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('password').value = '';
+  document.getElementById('password2').value = '';
   //   document.querySelector('.hidden-form').input.reset();
 });
